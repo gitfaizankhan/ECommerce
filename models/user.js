21e32a4fc0b6a4ea04ae68fb607bf9beb66a42d9
gitfaizankhan <faizankhaninfo9@gmail.com>
2023-04-27 10:54:50 +0530

    working on add to cart

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 const getDb = require('../util/database').getDb;
 const mongodb = require('mongodb');
 class User {
-  constructor(username, email){
+  constructor(username, email, cart, id){
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
@@ -12,6 +14,19 @@ class User {
     .collection('users')
     .insertOne(this);
   }
+
+  addToCart(product) {
+    const cartProduct = this.cart.items.findIndex(cp=>{
+      return cp._id === product._id;
+    });
+    const updateCart = { items: [{ ...product, quantity: 1}] };
+    const db = getDb();
+    return db.collection('users').updateOne(
+      {_id: new mongodb.ObjectId(this._id) },
+      {$set: {cart: updateCart}}
+    )
+  }
+
 
   static findById(userId) {
     const db = getDb();
